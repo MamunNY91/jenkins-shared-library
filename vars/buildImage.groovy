d7471fb5a5f4e6932fa4d80d2b92dee9ced10a78
Mamun <mamun1ny@gmail.com>
2022-11-23 00:23:31 -0500

    changed app version

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,8 +3,8 @@ def call()
     echo "Building the docker image"
     withCredentials([usernamePassword(credentialsId: 'docker-hub-cred',passwordVariable:'PASS',usernameVariable:'USER')])
             {
-                sh 'docker build -t samny91/springboot-docker-pipeline:1.0 .'
+                sh 'docker build -t samny91/springboot-docker-pipeline:1.2 .'
                 sh 'echo $PASS | docker login -u $USER --password-stdin'
-                sh "docker push samny91/springboot-docker-pipeline:1.0"
+                sh "docker push samny91/springboot-docker-pipeline:1.2"
             }
 }
